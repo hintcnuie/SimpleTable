4dc8ff0907ff81e46ac181570e54dbfc48dab4f3
hintcnuie <hintcnuie@sina.com>
2013-12-11 18:37:15 +0800

    update
    update SimpleTable sample code

--- SimpleTable/SimpleTableViewController.m
+++ SimpleTable/SimpleTableViewController.m
@@ -11,7 +11,7 @@
 
 @interface SimpleTableViewController ()
 {
-    NSArray *tableData;
+    NSMutableArray *tableData;
     NSArray *thumbnails;
     NSArray *prepTime;
 }
@@ -32,10 +32,19 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    tableData = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini",@"糖醋鲤鱼",@"九转大肠",@"汤爆双脆",@"百花大虾",@"蟹黄海参",@"德州扒鸡",@"神仙鸭子",@"一品豆腐", nil];
-    thumbnails =[NSArray arrayWithObjects:@"egg_benedict.jpg", @"mushroom_risotto.jpg", @"full_breakfast.jpg", @"hamburger.jpg", @"ham_and_egg_sandwich.jpg", @"creme_brelee.jpg", @"white_chocolate_donut.jpg", @"starbucks_coffee.jpg", @"vegetable_curry.jpg", @"instant_noodle_with_egg.jpg", @"noodle_with_bbq_pork.jpg", @"japanese_noodle_with_pork.jpg", @"green_tea.jpg", @"thai_shrimp_cake.jpg", @"angry_birds_cake.jpg", @"ham_and_cheese_panini.jpg", @"tangculiyu.jpg",@"jiuzhuandachang.jpg",@"tangbaoshuangcui.jpg",@"baihuadaxia.jpg",@"xiehuanghaishen.jpeg",@"dezhoupaji.jpg",@"shenxianyazi.jpeg",@"yipindoufu.jpg",nil];
-    // Initialize Preparation Time
-    prepTime = [NSArray arrayWithObjects:@"30 min", @"30 min", @"20 min", @"30 min", @"10 min", @"1 hour", @"45 min", @"5 min", @"30 min", @"8 min", @"20 min", @"20 min", @"5 min", @"1.5 hour", @"4 hours", @"10 min",@"0.5 hour",@"0.5 hour",@"0.5 hour",@"0.5 hour",@"0.5 hour",@"0.5 hour",@"0.5 hour",@"0.5 hour", nil];
+//    tableData = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini",@"糖醋鲤鱼",@"九转大肠",@"汤爆双脆",@"百花大虾",@"蟹黄海参",@"德州扒鸡",@"神仙鸭子",@"一品豆腐", nil];
+//    thumbnails =[NSArray arrayWithObjects:@"egg_benedict.jpg", @"mushroom_risotto.jpg", @"full_breakfast.jpg", @"hamburger.jpg", @"ham_and_egg_sandwich.jpg", @"creme_brelee.jpg", @"white_chocolate_donut.jpg", @"starbucks_coffee.jpg", @"vegetable_curry.jpg", @"instant_noodle_with_egg.jpg", @"noodle_with_bbq_pork.jpg", @"japanese_noodle_with_pork.jpg", @"green_tea.jpg", @"thai_shrimp_cake.jpg", @"angry_birds_cake.jpg", @"ham_and_cheese_panini.jpg", @"tangculiyu.jpg",@"jiuzhuandachang.jpg",@"tangbaoshuangcui.jpg",@"baihuadaxia.jpg",@"xiehuanghaishen.jpeg",@"dezhoupaji.jpg",@"shenxianyazi.jpeg",@"yipindoufu.jpg",nil];
+//    // Initialize Preparation Time
+//    prepTime = [NSArray arrayWithObjects:@"30 min", @"30 min", @"20 min", @"30 min", @"10 min", @"1 hour", @"45 min", @"5 min", @"30 min", @"8 min", @"20 min", @"20 min", @"5 min", @"1.5 hour", @"4 hours", @"10 min",@"0.5 hour",@"0.5 hour",@"0.5 hour",@"0.5 hour",@"0.5 hour",@"0.5 hour",@"0.5 hour",@"0.5 hour", nil];
+    
+    //Find out the path of recipes.plist
+    NSString *path =[[NSBundle mainBundle] pathForResource:@"recipes" ofType:@"plist"];
+    
+    //Load the file content and read the data into arrays
+    NSDictionary *dict=[[NSDictionary alloc] initWithContentsOfFile:path];
+    tableData =[dict objectForKey:@"RecipeName"];
+    thumbnails =[dict objectForKey:@"Thumbnail"];
+    prepTime =[dict objectForKey:@"PrepTime"];
 }
 
 - (void)didReceiveMemoryWarning
@@ -81,5 +90,9 @@
     UIAlertView *messageAlert = [[UIAlertView alloc] initWithTitle:@"Row Selected" message:messageStr delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
     
     [messageAlert show];
+}
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableData removeObjectAtIndex:indexPath.row];
 }
 @end
